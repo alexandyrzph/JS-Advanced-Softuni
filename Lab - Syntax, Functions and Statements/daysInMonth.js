853c174849b17531c1fde4c3a134c1940c1815c6
Alexander <58862704+alexandyrzph@users.noreply.github.com>
2021-12-25 11:36:15 +0200

    Update daysInMonth.js

--- Lab - Syntax, Functions and Statements/daysInMonth.js
+++ Lab - Syntax, Functions and Statements/daysInMonth.js
@@ -1,5 +1,5 @@
 function daysInMonth(month, year) {
-    return new Date(year, month ,0) .getDate();
+    return new Date(year, month, 0).getDate();
 }
 console.log(daysInMonth(1, 2012));
 console.log(daysInMonth(2, 2021));
